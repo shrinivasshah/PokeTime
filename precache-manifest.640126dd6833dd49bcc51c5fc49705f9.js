4e551a1872a737e6c246523ee60e9d08f96dc16b
shrinivasshah05 <shrinivasshah05@gmail.com>
2020-06-14 20:24:21 +0530

    Updates

---
+++ precache-manifest.640126dd6833dd49bcc51c5fc49705f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e53373eb996de01a41c055e5d4c49e2",
+    "url": "/PokeTime/index.html"
+  },
+  {
+    "revision": "bfd949171455e62a3433",
+    "url": "/PokeTime/static/css/main.6df0f788.chunk.css"
+  },
+  {
+    "revision": "2b64e4b4702face76ade",
+    "url": "/PokeTime/static/js/2.ea730946.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/PokeTime/static/js/2.ea730946.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfd949171455e62a3433",
+    "url": "/PokeTime/static/js/main.cbd59e4e.chunk.js"
+  },
+  {
+    "revision": "0e9be4222d880688d207",
+    "url": "/PokeTime/static/js/runtime-main.522772e3.js"
+  },
+  {
+    "revision": "f10bf0780aecd8ff951259976c1784dd",
+    "url": "/PokeTime/static/media/pokeball.f10bf078.png"
+  }
+]);
